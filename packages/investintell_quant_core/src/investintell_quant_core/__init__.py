aee90a4e5ca4c2c19b0bfa77fe4ea746233578d3
Andreicr1 <andrei.rachadel@outlook.com>
2026-06-25 21:34:09 -0300

    refactor: extract quant-core hashing

---
+++ packages/investintell_quant_core/src/investintell_quant_core/__init__.py
@@ -0,0 +1,6 @@
+"""Pure deterministic quant calculation core."""
+
+from .version import __version__
+
+__all__ = ["__version__"]
+
---
+++ packages/investintell_quant_core/src/investintell_quant_core/hashing/__init__.py
@@ -0,0 +1,6 @@
+"""Canonical hashing helpers."""
+
+from .canonical import logical_payload_hash, logical_records_hash, stable_hash
+
+__all__ = ["logical_payload_hash", "logical_records_hash", "stable_hash"]
+
